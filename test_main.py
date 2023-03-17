88d9c607d503abb326b6beacb293a1b76d4be43e
sofiaBasetis <sofia.martinez@basetis.com>
2023-03-17 17:08:08 +0100

    test

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from .main import app
+from main import app
 
 client = TestClient(app)
 
@@ -8,3 +8,26 @@ def test_mensaje():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello":"World"}
+
+
+
+def test_login():
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert response.json() == "Ingrese sus datos"
+
+
+def test_create_existing_item():
+    response = client.post(
+        "/students",
+        json={
+            "id": "000f",
+            "name": "Sofia",
+            "clase": 10
+            }
+    )
+    if response.status_code == 200: 
+        assert response.json() == "Estudiante registrado"
+    else:
+        assert response.json() == "Estudiante registrado"
+        
